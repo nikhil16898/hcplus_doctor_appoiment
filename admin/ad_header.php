6ba094f7536970084ff7d0f9bee0c4db0fc1bd70
Abhiratanpara <abhiratanpara.2004@gmail.com>
2023-09-04 21:51:40 +0530

    admin header

--- admin/ad_header.php
+++ admin/ad_header.php
@@ -12,18 +12,21 @@
   		<div class="ad_title">
   			<center><h1>HealthcarePlus-Admin Panale</h1></center>
   		</div>
-  		<div class="navbar">
-  			<nav>
-  				<ul>
-  					<li><a href="">home</a></li>
-  					<li><a href="">home</a></li>
-  					<li><a href="">home</a></li>
-  					<li><a href="">home</a></li>
-  					<li><a href="">home</a></li>
-  					<li><a href="">home</a></li>
-  				</ul>
-  			</nav>
-  		</div>
+  		  <div class="header" style='z-index:99;'>
+        <nav >
+        <img src="../img\logo.png" alt="logo">
+        <input type="checkbox" name="" id="click">
+        <label for="click" class="menu-btn">
+          <i class="fa-solid fa-bars"></i>
+        </label>
+                <ul>
+                    <li><a href="ad_doctor-appoiment.php">Appointments</a></li>
+                    <li><a href="upload_img.php">Upload photo</a></li>
+                    <li><a href="#">users</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+        </nav>
+    </div>
   	</div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
